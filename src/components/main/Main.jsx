05c92f4cd471a0085bb45f932d7440d0eb4d2a12
Johnny <jhnyc@connect.hku.hk>
2022-10-22 13:12:15 -0700

    wip

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -81,110 +81,162 @@ export default function Main() {
           }
         ></div>
         <div
-          className={
-            active
-              ? `block shoe-outline-${version} active`
-              : `block shoe-outline-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block shirt-outline1-${version} active`
-              : `block shirt-outline1-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block shirt-outline2-${version} active`
-              : `block shirt-outline2-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block shirt-outline3-${version} active`
-              : `block shirt-outline3-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block shirt-outline4-${version} active`
-              : `block shirt-outline4-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block shirt-outline5-${version} active`
-              : `block shirt-outline5-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block shoulder-outline1-${version} active`
-              : `block shoulder-outline1-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block shoulder-outline2-${version} active`
-              : `block shoulder-outline2-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block shoulder-outline3-${version} active`
-              : `block shoulder-outline3-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block shoulder-outline4-${version} active`
-              : `block shoulder-outline4-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block shoulder-outline5-${version} active`
-              : `block shoulder-outline5-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block leg-outline1-${version} active`
-              : `block leg-outline1-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block leg-outline2-${version} active`
-              : `block leg-outline2-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block jeans-outline1-${version} active`
-              : `block jeans-outline1-${version}`
-          }
-        ></div>
-        <div
-          className={
-            active
-              ? `block jeans-outline2-${version} active`
-              : `block jeans-outline2-${version}`
-          }
-        ></div>
+          className="outline"
+          style={{ opacity: version === "v4" ? "100" : "0" }}
+        >
+          <div
+            className={
+              active ? `block shoe-outline active` : `block shoe-outline`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block shirt-outline1 active` : `block shirt-outline1`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block shirt-outline2 active` : `block shirt-outline2`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block shirt-outline3 active` : `block shirt-outline3`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block shirt-outline4 active` : `block shirt-outline4`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block shirt-outline5 active` : `block shirt-outline5`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block shirt-outline6 active` : `block shirt-outline6`
+            }
+          ></div>
+          <div
+            className={
+              active
+                ? `block shoulder-outline1 active`
+                : `block shoulder-outline1`
+            }
+          ></div>
+          <div
+            className={
+              active
+                ? `block shoulder-outline2 active`
+                : `block shoulder-outline2`
+            }
+          ></div>
+          <div
+            className={
+              active
+                ? `block shoulder-outline3 active`
+                : `block shoulder-outline3`
+            }
+          ></div>
+          <div
+            className={
+              active
+                ? `block shoulder-outline4 active`
+                : `block shoulder-outline4`
+            }
+          ></div>
+          <div
+            className={
+              active
+                ? `block shoulder-outline5 active`
+                : `block shoulder-outline5`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block leg-outline1 active` : `block leg-outline1`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block leg-outline2 active` : `block leg-outline2`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block jeans-outline1 active` : `block jeans-outline1`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block jeans-outline2 active` : `block jeans-outline2`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block hat-outline1 active` : `block hat-outline1`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block hat-outline2 active` : `block hat-outline2`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block hat-outline3 active` : `block hat-outline3`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block hat-outline4 active` : `block hat-outline4`
+            }
+          ></div>
+          <div className={active ? `block hat-1 active` : `block hat-1`}></div>
+          <div className={active ? `block hat-2 active` : `block hat-2`}></div>
+          <div className={active ? `block hat-3 active` : `block hat-3`}></div>
+          <div
+            className={active ? `block hair-1 active` : `block hair-1`}
+          ></div>
+          <div
+            className={active ? `block hair-2 active` : `block hair-2`}
+          ></div>
+          <div
+            className={active ? `block hand-1 active` : `block hand-1`}
+          ></div>
+          <div
+            className={active ? `block hand-2 active` : `block hand-2`}
+          ></div>
+          <div
+            className={active ? `block hand-3 active` : `block hand-3`}
+          ></div>
+          <div
+            className={active ? `block hand-4 active` : `block hand-4`}
+          ></div>
+          <div
+            className={active ? `block hand-5 active` : `block hand-5`}
+          ></div>
+          <div
+            className={active ? `block hand-6 active` : `block hand-6`}
+          ></div>
+          <div
+            className={
+              active ? `block hand-fill-1 active` : `block hand-fill-1`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block hand-fill-2 active` : `block hand-fill-2`
+            }
+          ></div>
+          <div
+            className={
+              active ? `block hand-fill-3 active` : `block hand-fill-3`
+            }
+          ></div>
+        </div>
       </div>
     </div>
   );
