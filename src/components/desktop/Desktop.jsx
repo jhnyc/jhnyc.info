75c5558d4bb1c162565edcc284fbf6d44a50fbb9
Johnny <jhnyc@connect.hku.hk>
2022-11-11 15:10:18 -0800

    slide bg

--- src/components/desktop/Desktop.jsx
+++ src/components/desktop/Desktop.jsx
@@ -4,18 +4,11 @@ import Window from "./window/Window";
 import "./desktop.css";
 import WordAnimation from "../word_animation/WordAnimation";
 import projectsData from "../../assets/documents/projects.json";
+import { GrNext } from "react-icons/gr";
 
 export default function Desktop() {
-  const [windowZIndex, setWindowZIndex] = useState([0, 0, 0, 0, 0]);
-
-  const OpenHandler = (setStateFunc, index) => {
-    setStateFunc(true);
-    setWindowZIndex(
-      windowZIndex.map((v, ix) =>
-        ix == index ? Math.max(...windowZIndex) + 1 : v
-      )
-    );
-  };
+  const [color, setColor] = useState(0);
+  const [animate, setAnimate] = useState(0);
 
   const readmeContent = () => {
     return (
@@ -36,47 +29,68 @@ export default function Desktop() {
         <br />
         <p>Name: Johnny Chau</p>
         <br />
-        <p>From: Hong Kong</p>
+        <p>Location: Hong Kong --> San Francisco</p>
         <br />
         <p>Horoscope: I don’t believe in horoscopy.</p>
         <br />
         <p>Current: </p>
-        <p>&emsp; - Student @ USF Master of Science in Data Science</p>
+        <p>&emsp; - Student @ USF MS Data Science</p>
         <p>&emsp; - ML Intern @ Atlassian</p>
         <br />
         <p>
-          Description: Lorem ipsum, or lipsum as it is sometimes known, is dummy
-          text used in laying out print, graphic or web designs. The passage is
-          attributed to an unknown typesetter in the 15th century who is thought
-          to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for
-          use in a type specimen book.{" "}
+          Description: Data scientist/machine learning engineer by trade, front
+          end dev at heart. I studied accounting for my bachelor's, it's the
+          biggest mistake I've made.
         </p>
       </div>
     );
   };
 
+  const bgColors = ["#84C4F2", "#FFC700", "#534FF7", "F5F5F5"];
+
+  const colorHandler = () => {
+    setColor(color + 1);
+    setAnimate(1);
+  };
+
   return (
-    <div className="desktop">
-      <Window window={"picture"} />
-      <Window
-        window={"file"}
-        icon={"document"}
-        title={"readme.txt"}
-        content={readmeContent()}
-      />
-      <Window
-        window={"folder"}
-        title={"projects"}
-        color={"#FFC700"}
-        data={projectsData}
-      />
-      <Window window={"folder"} title={"untitled folder"} color={"#534FF7"} />
-      <Window window={"folder"} title={"photography"} color={"#0DAB58"} />
-      <Window window={"terminal"} title={"terminal"} />
-      <File name={"linkedin"} icon={"internet"} />
-      <File name={"github"} icon={"internet"} />
-      <File name={"email"} icon={"email"} />
-      <WordAnimation />
+    <div
+      className="desktop"
+      style={{ backgroundColor: bgColors[(color - 1) % bgColors.length] }}
+    >
+      <button id="changeBg" style={{ zIndex: "20" }} onClick={colorHandler}>
+        <GrNext size={35} />
+      </button>
+      <div
+        className="bg"
+        style={{ backgroundColor: bgColors[color % bgColors.length] }}
+        onAnimationEnd={() => setAnimate(2)}
+        animate={animate}
+      >
+        {" "}
+      </div>
+      <div className="desktop_container" style={{ zIndex: "1" }}>
+        <Window window={"picture"} />
+        <Window
+          window={"file"}
+          icon={"document"}
+          title={"readme.txt"}
+          content={readmeContent()}
+        />
+        <Window
+          window={"folder"}
+          title={"projects"}
+          color={"#FFC700"}
+          data={projectsData}
+        />
+        <Window window={"folder"} title={"untitled folder"} color={"#534FF7"} />
+        <Window window={"folder"} title={"photography"} color={"#0DAB58"} />
+        <Window window={"terminal"} title={"terminal"} />
+        <File name={"linkedin"} icon={"internet"} />
+        <File name={"github"} icon={"internet"} />
+        <File name={"email"} icon={"email"} />
+        <WordAnimation />
+      </div>
     </div>
   );
 }
