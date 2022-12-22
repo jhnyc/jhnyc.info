1e97866095cad6614c12799f4f23e549d7081e2d
Johnny <jhnyc@connect.hku.hk>
2022-12-22 15:09:07 -0800

    tweak

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { Navbar, Main, WordAnimation, Terminal, Desktop } from "./components";
-import Cmatrix from "./components/desktop/terminal/cmatrix/Cmatrix";
+import { Navbar, Desktop } from "./components";
 import "./App.css";
 
 function App() {
@@ -7,7 +6,6 @@ function App() {
     <div className="App">
       <Navbar />
       <Desktop />
-      {/* <Cmatrix /> */}
     </div>
   );
 }
